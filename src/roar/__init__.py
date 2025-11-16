ea9efbfb1b4ec20db144515895cb5680ae242844
Moritz <moritz.feik@gmail.com>
2025-11-16 16:49:20 +0100

    Explore structure of files

--- src/roar/__init__.py
+++ src/roar/__init__.py
@@ -1,3 +1,37 @@
 import pathlib
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent.parent
+DATA_DIR = ROOT_DIR / "data"
+
+ALL_VEHICLES = [
+    "eGolf",
+    "ID.4",
+    "Q8",
+    "Taycan",
+]
+
+VEHICLE_CLEAN_NAMES = {
+    "e-Golf": "eGolf",
+    "E-Golf": "eGolf",
+    "VW eGolf": "eGolf",
+    "ID4": "ID.4",
+    "VW ID4": "ID.4",
+    "Q8 e-tron": "Q8",
+    "AudiQ8": "Q8",
+    "Porsche": "Taycan",
+}
+
+ALL_TYRES = [
+    "EcoContact 6 Q",
+    "RainSport 5",
+    "PremiumContact 6 AO",
+    "P-Zero R",
+    "Ventus S1 evo 3 ev",
+    "Summer SRTT",
+]
+
+TYRE_CLEAN_NAMES = {
+    "PremiumContact 6": "PremiumContact 6 AO",
+    "Ventus S1 Evo 3": "Ventus S1 evo 3 ev",
+    "P-Zero": "P-Zero R",
+}
