c7c76a0bb000b55da51b9a5d7e558488f91f3205
Moritz <moritz.feik@gmail.com>
2025-11-27 19:11:48 +0100

    Adapt new data structure

--- src/roar/__init__.py
+++ src/roar/__init__.py
@@ -1,25 +1,28 @@
 import pathlib
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent.parent
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / "data_cleaned"
+EXTRAS_DIR = ROOT_DIR / "data_extras"
 
 ALL_VEHICLES = [
-    "eGolf",
-    "ID.4",
-    "Q8",
-    "Taycan",
+    "01 VW ID4",
+    "02 Audi Q8",
+    "03 Porsche Taycan",
+    "04 VW eGolf",
 ]
 
 VEHICLE_CLEAN_NAMES = {
-    "e-Golf": "eGolf",
-    "E-Golf": "eGolf",
-    "VW eGolf": "eGolf",
-    "ID4": "ID.4",
-    "VW ID4": "ID.4",
-    "Q8 e-tron": "Q8",
-    "AudiQ8": "Q8",
-    "Porsche": "Taycan",
+    "e-Golf": "04 VW eGolf",
+    "E-Golf": "04 VW eGolf",
+    "VW eGolf": "04 VW eGolf",
+    "ID4": "01 VW ID4",
+    "VW ID4": "01 VW ID4",
+    "Q8 e-tron": "02 Audi Q8",
+    "AudiQ8": "02 Audi Q8",
+    "Porsche": "03 Porsche Taycan",
 }
+
+ID_TO_VEHICLE = {i + 1: vehicle for i, vehicle in enumerate(ALL_VEHICLES)}
 
 ALL_TYRES = [
     "EcoContact 6 Q",
