41c99c8a521cfa9a1a0f84dfe8bffd1b30b01c9c
Moritz <moritz.feik@gmail.com>
2025-12-02 01:45:32 +0100

    Add analysis notebook

--- src/roar/__init__.py
+++ src/roar/__init__.py
@@ -11,6 +11,13 @@ ALL_VEHICLES = [
     "04 VW eGolf",
 ]
 
+VEHICLE_COLORS = {
+    "01 VW ID4": "#3498db",
+    "02 Audi Q8": "#e74c3c",
+    "03 Porsche Taycan": "#2ecc71",
+    "04 VW eGolf": "#9b59b6",
+}
+
 VEHICLE_CLEAN_NAMES = {
     "e-Golf": "04 VW eGolf",
     "E-Golf": "04 VW eGolf",
@@ -32,6 +39,15 @@ ALL_TYRES = [
     "Ventus S1 evo 3 ev",
     "Summer SRTT",
 ]
+
+TYRE_COLORS = {
+    1: "#e63946",
+    3: "#f4a261",
+    6: "#e9c46a",
+    10: "#2a9d8f",
+    12: "#264653",
+    13: "#7209b7",
+}
 
 TYRE_CLEAN_NAMES = {
     "PremiumContact 6": "PremiumContact 6 AO",
